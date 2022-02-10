2af06b7ca4290f33ccc302586a76af08d438eb76
caufieldjh <j.harry.caufield@gmail.com>
2022-02-10 18:18:16 -0500

    Transform function

--- run.py
+++ run.py
@@ -11,8 +11,11 @@ import os
 import glob
 import click
 
-from kgx.cli.cli_utils import transform
+import kgx.cli
 
+OUTDIR = "transformed"
+NAMESPACE = "data.bioontology.org"
+TARGET_TYPE = "ontologies"
 
 @click.command()
 @click.option("--input",
@@ -23,6 +26,7 @@ def run(input: str):
 
     data_filepaths = examine_data_directory(input)
     set_up_outdirs(data_filepaths)
+    do_transforms(data_filepaths)
 
 def examine_data_directory(input: str):
     """
@@ -52,19 +56,51 @@ def set_up_outdirs(paths: list) -> None:
     :param paths: list of file paths as strings
     """
 
-    outdir = "transformed"
-
     print("Setting up output directories...")
 
-    if not os.path.exists(outdir):
-        os.mkdir(outdir)
+    if not os.path.exists(OUTDIR):
+        os.mkdir(OUTDIR)
 
     for filepath in paths:
         newpath = "/".join((filepath.split("/"))[-3:-1])
-        newpath = os.path.join(outdir,newpath)
+        newpath = os.path.join(OUTDIR,newpath)
         print(newpath)
         if not os.path.exists(newpath):
             os.makedirs(newpath)
+
+def do_transforms(paths: list) -> None:
+    """
+    Given a list of file paths,
+    uses KGX to transform each file
+    to tsv node/edgelists.
+    Parses header for each to get
+    metadata.
+    :param paths: list of file paths as strings
+    """
+
+    print("Transforming...")
+
+    for filepath in paths:
+        with open(filepath) as infile:
+            header = (infile.readline()).rstrip()
+            metadata = (header.split(NAMESPACE))[1]
+            metadata = metadata.lstrip('/')
+            metadata_split = (metadata.split("/"))
+            if metadata_split[0] == TARGET_TYPE:
+                dataname = metadata_split[1]
+                version = metadata_split[3]
+                outname = f"{dataname}_{version}" 
+                outpath = os.path.join(OUTDIR,metadata[0:2],outname)
+            else:
+                continue
+        
+        # Still need to remove first line or KGX will choke
+
+        kgx.cli.transform(inputs=filepath,
+                          input_format='nt',
+                          output=outpath,
+                          output_format='tsv')
+            
 
 if __name__ == '__main__':
   run()
