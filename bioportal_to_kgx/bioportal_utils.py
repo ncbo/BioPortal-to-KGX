dc61447610cb35ea3f0654d9efd7088688412b1f
caufieldjh <j.harry.caufield@gmail.com>
2022-03-23 11:57:06 -0400

    Use NCBO API key - not quite working yet

--- bioportal_to_kgx/bioportal_utils.py
+++ bioportal_to_kgx/bioportal_utils.py
@@ -1,12 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def bioportal_metadata(outname: str, outdir: str) -> None:
+import requests
+
+BASE_ONTO_URL = "https://data.bioontology.org/ontologies/"
+
+def bioportal_metadata(outname: str, outdir: str, api_key: str) -> None:
     """
     Retrieve metadata for the given ontology.
+    Note that this requires a NCBO API key,
+    to be passed in api_key.
     Writes metadata as nodelist to outdir.
     :param outname: short identifier for the ontology,
                     to be used for API calls
     :param outdir: directory to write outfile to
+    :param api_key: str, NCBO API key
     """
-    pass
+
+    req_url = BASE_ONTO_URL + outname
+    params = dict(key=api_key)
+
+    print(f"Accessing {req_url}...")
+
+    response = requests.get(req_url, params=params)
+    print(response)
+
+    content = response.json()
