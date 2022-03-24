6090b14c88ff212260238f77f56f1a2da3ee6ed7
caufieldjh <j.harry.caufield@gmail.com>
2022-03-24 16:13:02 -0400

    Minor changes and comments

--- bioportal_to_kgx/bioportal_utils.py
+++ bioportal_to_kgx/bioportal_utils.py
@@ -5,19 +5,21 @@ import requests # type: ignore
 
 BASE_ONTO_URL = "https://data.bioontology.org/ontologies/"
 
-def bioportal_metadata(outname: str, outdir: str, api_key: str) -> None:
+def bioportal_metadata(ontoid: str, outdir: str, api_key: str) -> None:
     """
     Retrieve metadata for the given ontology.
     Note that this requires a NCBO API key,
     to be passed in api_key.
-    Writes metadata as nodelist to outdir.
+    Writes metadata to the ontology's nodelist.
     :param outname: short identifier for the ontology,
                     to be used for API calls
     :param outdir: directory to write outfile to
     :param api_key: str, NCBO API key
     """
 
-    req_url = BASE_ONTO_URL + outname
+    # Return content from the Ontology endpoint
+    # http://data.bioontology.org/metadata/Ontology
+    req_url = BASE_ONTO_URL + ontoid
     params = dict(apikey=api_key,display_context="False",include="all")
 
     print(f"Accessing {req_url}...")
@@ -26,4 +28,10 @@ def bioportal_metadata(outname: str, outdir: str, api_key: str) -> None:
     print(response)
 
     content = response.json()
-    print(content['name'])
+    print(f"Retrieved metadata for {ontoid} ({content['name']})")
+
+    # Load all nodes with kgx.transformer
+    # Set 'provided_by' to the name of the ontology
+    # then store the object
+    # then save the object over the previous nodelist
+    # or just use the kgx.cli with the --knowledge-sources flag?
