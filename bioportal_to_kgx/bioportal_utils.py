63f7fd3fbbe2c5ab5f4fa844e23246c34060a878
caufieldjh <j.harry.caufield@gmail.com>
2022-03-30 12:05:26 -0400

    Check for existing metadata in transforms

--- bioportal_to_kgx/bioportal_utils.py
+++ bioportal_to_kgx/bioportal_utils.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 import requests # type: ignore
 
 BASE_ONTO_URL = "https://data.bioontology.org/ontologies/"
 
-def bioportal_metadata(ontoid: str, outdir: str, api_key: str) -> None:
+def bioportal_metadata(ontoid: str, api_key: str) -> dict:
     """
     Retrieve metadata for the given ontology.
     Note that this requires a NCBO API key,
     to be passed in api_key.
-    Writes metadata to the ontology's nodelist.
+    Returns a dict.
     :param outname: short identifier for the ontology,
                     to be used for API calls
     :param outdir: directory to write outfile to
     :param api_key: str, NCBO API key
     """
-
+    desired_metadata = ['name']
+    md = {key: None for key in desired_metadata}
+        
     # Return content from the Ontology endpoint
     # http://data.bioontology.org/metadata/Ontology
     req_url = BASE_ONTO_URL + ontoid
@@ -28,10 +31,37 @@ def bioportal_metadata(ontoid: str, outdir: str, api_key: str) -> None:
     print(response)
 
     content = response.json()
-    print(f"Retrieved metadata for {ontoid} ({content['name']})")
+    if 'status' in content: #API doesn't return status on 200
+        content = None
+    
+    # Reduce the content to just what we want
+    if content:
+        for md_type in desired_metadata:
+            md[md_type] = content[md_type]
 
-    # Load all nodes with kgx.transformer
-    # Set 'provided_by' to the name of the ontology
-    # then store the object
-    # then save the object over the previous nodelist
-    # or just use the kgx.cli with the --knowledge-sources flag?
+        print(f"Retrieved metadata for {ontoid} ({md['name']})")
+    
+    else:
+        print(f"Could not retrieve metadata for {ontoid}.")
+
+    return md
+
+def check_header_for_md(filepath: str) -> bool:
+    """
+    Given a filename for a KGX edge or nodelist,
+    checks for presence of metadata property names.
+    :param filepath: str, path to KGX format file
+    :return: bool, True if metadata fiels appear present
+    """
+
+    have_md = False
+
+    md_headings = ['primary_knowledge_source']
+    with open(filepath,'r') as infile:
+        header = infile.readline()
+    
+    for heading in md_headings:
+        if heading in header:
+            have_md = True
+
+    return have_md
