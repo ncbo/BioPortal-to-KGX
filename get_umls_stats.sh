7bf1845ea351baa15dd751b45ce145a749bde7e6
caufieldjh <j.harry.caufield@gmail.com>
2022-04-20 15:41:07 -0400

    Minor text output change

--- get_umls_stats.sh
+++ get_umls_stats.sh
@@ -14,7 +14,7 @@ grep -r -m1 --include \*_edges.tsv $STY_PREFIX $TX_PATH | awk -F/ '{print $4}'
 echo "*** Ontologies with nodes involving UMLS semantic types:"
 grep -r -m1 --include \*_nodes.tsv $STY_PREFIX $TX_PATH | awk -F/ '{print $4}'
 
-echo "*** The following codes are used:"
+echo "*** The following codes are used, followed by their usage (combined nodes+edges):"
 grep -roh --include \*.tsv "$STY_PREFIX...." $TX_PATH | sort | uniq -c | sort -rn
 
 echo "Complete."
