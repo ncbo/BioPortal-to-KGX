ba13b712add69e1c9b24ac381015ec1cf437ce91
caufieldjh <j.harry.caufield@gmail.com>
2022-05-10 15:30:53 -0400

    Mostly working IRI enumeration script

--- get_iris.sh
+++ get_iris.sh
@@ -13,8 +13,41 @@ for file in $NODE_FILES
 do
     dirpath=${file%/*}/
     ontology=$(basename "$dirpath" | tr -d '\n')
-    prefix=$(cut -f1 $file)
-    delimiter=$(printf "a_delimiter")
-    native=$(printf "true_or_false")
-    printf "$ontology\t$prefix\t$delimiter\t$native\n"
+    prefixes=$(cut -f1 $file | rev | cut -d"#" -f2- | rev | sort | uniq)
+    delimiter=$(printf "#")
+
+    # If this delimiter didn't appear to work, try another
+    prefix_count=$(wc -w <<< "$prefixes")
+    too_many=10
+    if (( $prefix_count > $too_many ))
+    then
+        prefixes=$(cut -f1 $file | rev | cut -d"/" -f2- | rev | sort | uniq)
+        delimiter=$(printf "/")
+    fi
+
+    # If this delimiter didn't appear to work, try another
+    # Keeping in mind that : is the standard CURIE delimiter
+    # at least for OBO imports
+    prefix_count=$(wc -w <<< "$prefixes")
+    too_many=10
+    if (( $prefix_count > $too_many ))
+    then
+        prefixes=$(cut -f1 $file | rev | cut -d":" -f2- | rev | sort | uniq)
+        delimiter=$(printf ":")
+    fi
+
+    # Ran out of standard delimiters, so we may have some strangeness in IRI format
+    prefix_count=$(wc -w <<< "$prefixes")
+    too_many=100
+    if (( $prefix_count > $too_many ))
+    then
+        prefixes=$(cut -f1 $file | rev | cut -f2 | rev | sort | uniq)
+        delimiter=$(printf "OTHER")
+    fi
+
+    for prefix in $prefixes
+    do 
+        native=$(printf "unknown")
+        printf "$ontology\t$prefix\t$delimiter\t$native\n"
+    done
 done
